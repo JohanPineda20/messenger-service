fe40e3a559b7a2953591a059ec1bbf3040c4cbb4
Johan Pineda <mpineda348@gmail.com>
2023-09-27 12:46:08 -0500

    documentation done

--- src/main/java/com/pragma/messengerservice/infraestructure/input/rest/MessageController.java
+++ src/main/java/com/pragma/messengerservice/infraestructure/input/rest/MessageController.java
@@ -2,6 +2,13 @@ package com.pragma.messengerservice.infraestructure.input.rest;
 
 import com.pragma.messengerservice.application.dto.MessageRequest;
 import com.pragma.messengerservice.application.handler.IMessageHandler;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -12,12 +19,22 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Messenger Controller")
 @RestController
 @RequestMapping("/message")
 @RequiredArgsConstructor
 public class MessageController {
 
     private final IMessageHandler messageHandler;
+
+    @Operation(summary = "Send sms to customers' cellphones")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "sms sent", content = @Content),
+            @ApiResponse(responseCode = "400", description = "Bad request: wrong input data", content = @Content(mediaType = "application/json", schema = @Schema(ref = "#/components/schemas/Exception"))),
+            @ApiResponse(responseCode = "403", description = "Forbidden", content = @Content(mediaType = "application/json", schema = @Schema(ref = "#/components/schemas/Exception"))),
+            @ApiResponse(responseCode = "500", description = "Twilio Error", content = @Content(mediaType = "application/json", schema = @Schema(ref = "#/components/schemas/Exception"))),
+    })
+    @SecurityRequirement(name = "jwt")
     @PostMapping
     @PreAuthorize("hasAuthority('EMPLOYEE')")
     public ResponseEntity<Void> sendMessage(@Valid @RequestBody MessageRequest messageRequest){
